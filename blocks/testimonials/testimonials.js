989da7585d1daaa08ab726831b062a5f6dec99b0
Lamont Crook <lamont@adobe.com>
2025-08-26 12:25:49 -0600

    update

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -7,14 +7,18 @@ export default function decorate(block) {
   slider.classList.add('slider');
   let i = 1;
 
+
   [...block.children].forEach((row) => {
     row.setAttribute('id', `slide${i}`);
     row.classList.add('slide');
     slider.append(row);
-    const a = document.createElement('a');
-    a.setAttribute('href', `#slide${i}`);
-    block.append(a);
+    if (templates.length > 1) {
+      const a = document.createElement('a');
+      a.setAttribute('href', `#slide${i}`);
+      block.append(a);
+    }
     i += 1;
   });
+
   block.prepend(slider);
 }
